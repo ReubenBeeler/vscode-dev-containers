179ab4513d71f786a21a498475f54d38ad6b8a60
ReubenBeeler <reubenfbeeler@gmail.com>
2026-03-14 23:10:24 -0600

    add ruflo for agent orchestration

--- ubuntu/postCreate.sh
+++ ubuntu/postCreate.sh
@@ -7,7 +7,23 @@ set -e
 # └─────┘
 
 curl -fsSL https://bun.sh/install | bash	# Auto-adds bun to PATH in ~/.bashrc
-export PATH="$HOME/.bun/bin:$PATH"			# Add bun to PATH for this current shell
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# ┌──────┐
+# │ Node │ because ruflo hooks use node (even though we install ruflo with bun)
+# └──────┘
+
+# ruflo recommends fnm
+curl -fsSL https://fnm.vercel.app/install | bash	# Auto-adds bun to PATH in ~/.bashrc
+
+FNM_PATH="/home/vscode/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="$FNM_PATH:$PATH"
+  eval "$(fnm env --shell bash)"
+fi
+
+fnm install 20
 
 # ┌─────────────┐
 # │ Claude Code │
@@ -38,7 +54,11 @@ curl -fsSL https://claude.ai/install.sh | bash
 
 ## Claude Code Extensions
 
+# agent orchestration
+bunx ruflo init
+
+# status line
 bunx --yes @kamranahmedse/claude-statusline
 
-# claude-in-mobile MCP server (https://github.com/AlexGladkov/claude-in-mobile)
+# mobile dev MCP server (https://github.com/AlexGladkov/claude-in-mobile)
 claude mcp add --scope user --transport stdio mobile -- bunx -y claude-in-mobile
