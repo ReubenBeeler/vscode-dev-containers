803a726f2bef1a828265ea4b3e341f571562df12
ReubenBeeler <reubenfbeeler@gmail.com>
2026-03-18 19:04:36 -0600

    remove ruflo cuz it's broken

--- ubuntu/postCreate.sh
+++ ubuntu/postCreate.sh
@@ -18,21 +18,6 @@ curl -fsSL https://bun.sh/install | bash	# Auto-adds bun to PATH in ~/.bashrc
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
-# ┌──────┐
-# │ Node │ because ruflo hooks use node (even though we install ruflo with bun)
-# └──────┘
-
-# ruflo recommends fnm
-curl -fsSL https://fnm.vercel.app/install | bash	# Auto-adds bun to PATH in ~/.bashrc
-
-FNM_PATH="/home/vscode/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="$FNM_PATH:$PATH"
-  eval "$(fnm env --shell bash)"
-fi
-
-fnm install 20
-
 # ┌─────────────┐
 # │ Claude Code │
 # └─────────────┘
@@ -62,15 +47,4 @@ curl -fsSL https://claude.ai/install.sh | bash
 
 ## Claude Code Extensions
 
-# ruflo – agent orchestration
-bunx ruflo init < <(echo No) || true # don't overwrite config files if they already exist
-bunx ruflo daemon start
-bunx ruflo memory configure --backend hybrid
-bunx ruflo memory init || true		 # it might fail if one already exists, which is fine
-claude mcp add claude-flow bunx @claude-flow/cli@v3alpha mcp start
-bun install -D typescript
-bun install agentic-flow@latest
-bunx ruflo doctor
-
-# mobile dev MCP server (https://github.com/AlexGladkov/claude-in-mobile)
-claude mcp add --scope user --transport stdio mobile -- bunx -y claude-in-mobile
+bunx --yes @kamranahmedse/claude-statusline
